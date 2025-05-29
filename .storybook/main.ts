bc3e4ced4602a9106c56d1126c0889c003a77d08
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-05-29 10:23:42 +0200

    aggiunti addons per accessibilità e per il test della viewport

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,27 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
-    "@storybook/addon-themes"
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        viewport: true, 
+      },
+    },
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
+    '@storybook/addon-themes',
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport', 
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
 };
+
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,17 @@
 import type { Preview } from "@storybook/react";
 import { withThemeByClassName } from "@storybook/addon-themes";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import "../src/styles/reset.css";
 import "../src/styles/variables.css";
 import "../src/styles/typography.css";
 
-
-
 const preview: Preview = {
   parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'responsive',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
